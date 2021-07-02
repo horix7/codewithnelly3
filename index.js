e13439be75b7a945f5f37e0c8f1364bb04607d7d
horix7 <horix77777@gmail.com>
2021-07-02 16:50:01 +0100

    removed proxy and cors resolving

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ import routes from './src/route'
 import express from "express"
 import path from "path"
 
+
 const app = express()
 const PORT = process.env.PORT || 1000
 
+app.use((request, response, next) => {
+
+  res.setHeader('Access-Control-Allow-Origin', 'https://nellychallenge1.web.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+
+})
 app.get("/", (request, response) => {
   response.sendFile(path.join(__dirname+'/views/index.html'));
 })
+
 
 app.use(routes)
 
